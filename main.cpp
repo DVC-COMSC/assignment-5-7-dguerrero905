d726eb634d1b37cf81b8b8f17e62bcc9f9269189
dguerrero905 <158696989+dguerrero905@users.noreply.github.com>
2024-03-22 21:50:34 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,28 +4,28 @@ using namespace std;
 int main() {
     int first, second, N;
 
-    // Input the first two initial values
+    // Ask for the first two initial values
     cout << "Enter the first initial value: ";
     cin >> first;
     cout << "Enter the second initial value: ";
     cin >> second;
 
-    // Input the number of sequences
+    // Ask for the number of sequences
     cout << "Enter the number of sequences (N > 2): ";
     cin >> N;
 
     // Print the first two initial values
-    cout << "Sequence of summation values:" << endl;
     cout << first << " " << second << " ";
 
     // Generate the sequence of numbers
+    int prev = first, curr = second;
     for (int i = 2; i < N; ++i) {
-        int next = first + second;
+        int next = prev + curr;
         cout << next << " ";
 
-        // Update first and second for the next iteration
-        first = second;
-        second = next;
+        // Update previous and current for the next iteration
+        prev = curr;
+        curr = next;
     }
 
     cout << endl;
